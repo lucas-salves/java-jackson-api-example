2084933e5b2bdcf90f014674f223c4382d1bb216
lucas-salves <lucas.salves@terra.com>
2021-09-28 19:27:58 -0300

    first commit

---
+++ src/main/java/JavaHttpgetapi/JavaHttpgetapi/JavaHttpGetApiApplication.java
@@ -0,0 +1,14 @@
+package JavaHttpgetapi.JavaHttpgetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHttpGetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHttpGetApiApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/JavaHttpgetapi/JavaHttpgetapi/JavaHttpGetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaHttpgetapi.JavaHttpgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHttpGetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
